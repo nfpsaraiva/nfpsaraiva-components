0072cd4eeda9b292461b0e5b6cfabeeca93d19e0
Nuno Saraiva <nuno.saraiva@wacestudio.com>
2024-06-15 19:52:13 +0100

    testing

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,23 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'nfpsaraiva-compoments', // Name your library here
+    },
+    rollupOptions: {
+      // Externalize dependencies that your library doesn't provide
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use when importing the library
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
